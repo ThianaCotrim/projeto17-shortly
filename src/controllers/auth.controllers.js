3f5a6ccfcc511a1870b90bd6d4ccf938a4cf3a72
Thiana <thianacantunes@gmail.com>
2023-05-17 20:16:40 -0300

    Version fixing the email

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -5,8 +5,9 @@ export async function singUp (req, res) {
     const {name, email, password, confirmPassword} = req.body
 
     try {
-        await db.query(`INSERT INTO clientes (name, email, password, confirm_password) VALUES ($1, $2, $3, $4);`,
+        await db.query(`INSERT INTO clientes (name, email, password, "confirmPassword") VALUES ($1, $2, $3, $4);`,
         [name, email, password, confirmPassword])
+      
         res.status(201).send("Cliente cadastrado com sucesso no banco de dados")
     } catch (err){
         res.status(500).send(err.message)
