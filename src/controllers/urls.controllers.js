6ce02a3d8568160fccfe8dab919dcafe94cbf6b0
Thiana <thianacantunes@gmail.com>
2023-05-18 16:11:36 -0300

    Ultimo teste do token invalido

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -17,7 +17,7 @@ export async function insertShortUrl (req, res,) {
         //     return res.status(422).send("A URL é obrigatória no corpo da requisição.");
         //   }
 
-        const id = await db.query(`SELECT * FROM login WHERE token = '6283d3c1-beb7-4d14-bcf8-36f45fd910f7'`)
+        const id = await db.query(`SELECT * FROM login WHERE token = 6283d3c1-beb7-4d14-bcf8-36f45fd910f7`)
         const idUser = id.rows[0].idUsuario
 
         const shortlyUrl = nanoid()
