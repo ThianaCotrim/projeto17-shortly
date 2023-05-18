2b6d26c2d60d48d9e2ad3b62ade679ded938a773
Thiana <thianacantunes@gmail.com>
2023-05-18 15:45:46 -0300

    Implementando o post de shortly

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -20,7 +20,7 @@ export async function singUp (req, res) {
     }
 }
 
-export async function signIn (req, res) {
+export async function signIn (req, res, token) {
 
     const {email, password} = req.body
 
--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -2,20 +2,28 @@
 import { db } from "../database/database.connection.js"
 import { nanoid } from "nanoid"
 
-export async function insertShortUrl (req, res) {
+export async function insertShortUrl (req, res,) {
 
     const {url} = req.body
+    const { authorization } = req.headers
+    
+    const token = authorization?.replace("Bearer ", "")
+
 
     try {
 
-        if (!url) {
-            return res.status(422).send("A URL é obrigatória no corpo da requisição.");
-          }
+        // if (!url) {
+        //     return res.status(422).send("A URL é obrigatória no corpo da requisição.");
+        //   }
+
+        const id = await db.query(`SELECT * FROM login WHERE token=$1`, [token])
+        const idUsuario = id.rows[0].idUsuario
 
         const shortlyUrl = nanoid()
-        const encurtador = await db.query(`INSERT INTO shorts (shortlyUrl, url) VALUES ($1, $2)`, [shortlyUrl, url])
+        await db.query(`INSERT INTO encurtar ("urlOriginal", "urlEncurtada", "criadorDaUrl") 
+        VALUES ($1, $2, $3)`, [url, shortlyUrl, idUsuario])
 
-        res.send(encurtador)
+        res.status(201).send(idUsuario, shortlyUrl)
 
     } catch (err){
         res.status(500).send(err.message)
