cf00de8790d54772c3c871f832c71ae8253f18fa
Thiana <thianacantunes@gmail.com>
2023-05-18 16:15:55 -0300

    Teste

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -17,8 +17,12 @@ export async function insertShortUrl (req, res,) {
         //     return res.status(422).send("A URL é obrigatória no corpo da requisição.");
         //   }
 
-        const id = await db.query(`SELECT * FROM login WHERE token = 6283d3c1-beb7-4d14-bcf8-36f45fd910f7`)
+        const id = await db.query(`SELECT * FROM login WHERE token=$1`, [token])
+       
         const idUser = id.rows[0].idUsuario
+        if (!id || id.rows.length === 0) {
+        return res.status(401).send('Token de autenticação inválido ou não encontrado.')}
+
 
         const shortlyUrl = nanoid()
         await db.query(`INSERT INTO encurtar ("urlOriginal", "urlEncurtada", "criadorDaUrl") 
