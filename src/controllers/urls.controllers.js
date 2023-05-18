3549c3c5820c706efd7ac73db01200617985eafd
Thiana <thianacantunes@gmail.com>
2023-05-18 16:31:10 -0300

    testando

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -7,22 +7,12 @@ export async function insertShortUrl (req, res,) {
     const {url} = req.body
     const { authorization } = req.headers
     
-    const token = authorization?.replace("Bearer ", "")
-    console.log(token)
-
+    const token = authorization?.replace("Bearer ", '')
 
     try {
 
-        // if (!url) {
-        //     return res.status(422).send("A URL é obrigatória no corpo da requisição.");
-        //   }
-
-        const id = await db.query(`SELECT * FROM login WHERE token=$1`, [token])
-       
+        const id = await db.query(`SELECT * FROM login WHERE token= 'a32b21a6-76d3-45e8-9ccb-4a698bdf671f'`)
         const idUser = id.rows[0].idUsuario
-        if (!id || id.rows.length === 0) {
-        return res.status(401).send('Token de autenticação inválido ou não encontrado.')}
-
 
         const shortlyUrl = nanoid()
         await db.query(`INSERT INTO encurtar ("urlOriginal", "urlEncurtada", "criadorDaUrl") 
