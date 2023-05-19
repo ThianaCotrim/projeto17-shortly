72304dc01c70d0a3e6d197c58f4f0f82492a05c6
Thiana <thianacantunes@gmail.com>
2023-05-18 21:30:52 -0300

    Teste final de hoje

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -54,7 +54,7 @@ export async function getOpenUrl (req, res,) {
 
         await db.query(`UPDATE encurtar SET "contagemVisitas" = "contagemVisitas" + 1 WHERE "urlEncurtada" = $1`, [shortUrl])
 
-        res.redirect(200, urlEncurtada)
+        res.redirect(302, urlEncurtada)
 
     }catch (err) {
         res.status(500).send(err.message)
