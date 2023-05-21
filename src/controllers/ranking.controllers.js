0cee0b1b6fc28e548c6b85a63eaa73fe8d10801d
Thiana <thianacantunes@gmail.com>
2023-05-21 08:38:33 -0300

    Sem conseguir fazer o dump

--- src/controllers/ranking.controllers.js
+++ src/controllers/ranking.controllers.js
@@ -8,12 +8,14 @@ export async function ranking (req, res,) {
           FROM encurtar e
           JOIN clientes l ON e."criadorDaUrl"::text = l.id::text
           GROUP BY e."criadorDaUrl", l."name"
+          LIMIT 10
         `);
     
         const visitas = await db.query(`
           SELECT "criadorDaUrl", SUM("contagemVisitas") AS "totalVisitas"
           FROM encurtar
           GROUP BY "criadorDaUrl"
+          LIMIT 10
         `);
         const totalVisitas = visitas.rows;
     
@@ -28,8 +30,11 @@ export async function ranking (req, res,) {
             visitCount: visitCount
           };
         });
+
+        allDados.sort((a, b) => b.visitCount - a.visitCount);
+        const ultimos10 = allDados.slice(-10);
         
-        res.status(200).send(allDados)
+        res.status(200).send(ultimos10)
     } catch (err){
         res.status(500).send(err.message)
     }
