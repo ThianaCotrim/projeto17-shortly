25a9739d25e935cbab8a260f0686b9af0ed90e50
Thiana <thianacantunes@gmail.com>
2023-05-22 15:32:58 -0300

    teste

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -10,64 +10,88 @@ export async function getDadosUsuario (req, res,) {
 
     try {
 
-    //     const confirmToken = await db.query(`SELECT * FROM login WHERE token=$1`, [token])
-    //     const usuarioId = confirmToken.rows[0].idUsuario
+        const confirmToken = await db.query(`SELECT * FROM login WHERE token=$1`, [token])
+        const usuarioId = confirmToken.rows[0].idUsuario
 
-    //     const nomeUsuario = await db.query(`SELECT * FROM clientes WHERE id=$1`, [usuarioId])
+        const nomeUsuario = await db.query(`SELECT * FROM clientes WHERE id=$1`, [usuarioId])
 
-    //     const pegarDados = await db.query(`SELECT id, "urlOriginal", "urlEncurtada", "contagemVisitas" FROM encurtar WHERE "criadorDaUrl"=$1`, [usuarioId])
-    //     const todosOsDados = pegarDados.rows
+        const pegarDados = await db.query(`SELECT id, "urlOriginal", "urlEncurtada", "contagemVisitas" FROM encurtar WHERE "criadorDaUrl"=$1`, [usuarioId])
+        const todosOsDados = pegarDados.rows
 
-    // const objeto = {
-    //     id: confirmToken.rows[0].idUsuario, 
-    //     name: nomeUsuario.rows[0].name, 
-    //     visitCount: somaVisitas, 
-    //     shortenedUrls: todosOsDados.map(item =>({
-    //         id: item.id,
-    //         shortUrl: item.urlEncurtada,
-    //         url: item.urlOriginal,
-    //         visitCount: item.contagemVisitas
-    //     }))
-    // }
-    // console.log(todosOsDados)
+        const allDados = nomeUsuario.rows.map(all => {
+                    const id = confirmToken.rows.find(i => i.idUsuario)
+                    const name = nomeUsuario.rows.find(n => n.name)
+                    return {
+                        id: id.idUsuario,
+                        name: name.name,
+                        visitCount: 0,
+                        shortenedUrls: todosOsDados
+                    }
+                })
+                
+                let somaVisitas = 0;
+                allDados[0].shortenedUrls.forEach(url => {
+                somaVisitas += url.contagemVisitas;
+                allDados[0].visitCount = somaVisitas
+            });
+
+        
+
+    const objeto = {
+        id: confirmToken.rows[0].idUsuario, 
+        name: nomeUsuario.rows[0].name, 
+        visitCount: somaVisitas, 
+        shortenedUrls: todosOsDados.map(item =>({
+            id: item.id,
+            shortUrl: item.urlEncurtada,
+            url: item.urlOriginal,
+            visitCount: item.contagemVisitas
+        }))
+    }
+    console.log(todosOsDados)
    
 
-    //     res.status(200).send(objeto)
+        res.status(200).send(objeto)
 
-    const confirmToken = await db.query(`SELECT * FROM login WHERE token=$1`, [token])
-    const usuarioId = confirmToken.rows[0].idUsuario
-    const nomeUsuario = await db.query(`SELECT * FROM clientes WHERE id=$1`, [usuarioId])
+//     const confirmToken = await db.query(`SELECT * FROM login WHERE token=$1`, [token])
+//     const usuarioId = confirmToken.rows[0].idUsuario
+//     const nomeUsuario = await db.query(`SELECT * FROM clientes WHERE id=$1`, [usuarioId])
 
-    const pegarDados = await db.query(`SELECT id, "urlOriginal", "urlEncurtada", "contagemVisitas" FROM encurtar WHERE "criadorDaUrl"=$1`, [usuarioId])
-    const todosOsDados = pegarDados.rows
-    console.log(todosOsDados)
+//     const pegarDados = await db.query(`SELECT id, "urlOriginal", "urlEncurtada", "contagemVisitas" FROM encurtar WHERE "criadorDaUrl"=$1`, [usuarioId])
+//     const todosOsDados = pegarDados.rows
+//     console.log(todosOsDados)
 
-    const totalLinks = pegarDados.rowCount
+//     const totalLinks = pegarDados.rowCount
 
-    const allDados = nomeUsuario.rows.map(all => {
-        const id = confirmToken.rows.find(i => i.idUsuario)
-        const name = nomeUsuario.rows.find(n => n.name)
-        return {
-            id: id.idUsuario,
-            name: name.name,
-            visitCount: 0,
-            shortenedUrls: todosOsDados
-        }
-    })
+//     const allDados = nomeUsuario.rows.map(all => {
+//         const id = confirmToken.rows.find(i => i.idUsuario)
+//         const name = nomeUsuario.rows.find(n => n.name)
+//         return {
+//             id: id.idUsuario,
+//             name: name.name,
+//             visitCount: 0,
+//             shortenedUrls: todosOsDados
+//         }
+//     })
     
-    let somaVisitas = 0;
-    allDados[0].shortenedUrls.forEach(url => {
-    somaVisitas += url.contagemVisitas;
-    allDados[0].visitCount = somaVisitas
-});
+//     let somaVisitas = 0;
+//     allDados[0].shortenedUrls.forEach(url => {
+//     somaVisitas += url.contagemVisitas;
+//     allDados[0].visitCount = somaVisitas
+// });
 
-const objeto = {id: confirmToken.rows[0].idUsuario, name: nomeUsuario.rows[0].name, visitCount: somaVisitas, shortenedUrls: todosOsDados}
-console.log(objeto)
+// const objeto = 
 
 
-    res.status(200).send(objeto)
+//     const objeto = {id: confirmToken.rows[0].idUsuario, name: nomeUsuario.rows[0].name, visitCount: somaVisitas, shortenedUrls: todosOsDados}
+//     console.log(objeto)
+
+
+//     res.status(200).send(objeto)
     } catch (err){
         res.status(500).send(err.message)
+        console.log(err)
     }
+    
 
 }
