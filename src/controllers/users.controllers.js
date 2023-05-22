aadc53ef921a95d4c9946d2b0fcf60f1219df223
Thiana <thianacantunes@gmail.com>
2023-05-22 15:11:24 -0300

    testando

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -5,8 +5,8 @@ export async function getDadosUsuario (req, res,) {
     const { authorization } = req.headers
     const token = authorization?.replace("Bearer ", "")
 
-    const sessao = await db.query(`SELECT * FROM login WHERE token=$1`, [token])
-    if (sessao.rows.length === 0) return res.sendStatus(401)
+    // const sessao = await db.query(`SELECT * FROM login WHERE token=$1`, [token])
+    // if (sessao.rows.length === 0) return res.sendStatus(401)
 
     try {
 
@@ -17,33 +17,6 @@ export async function getDadosUsuario (req, res,) {
 
         const pegarDados = await db.query(`SELECT id, "urlOriginal", "urlEncurtada", "contagemVisitas" FROM encurtar WHERE "criadorDaUrl"=$1`, [usuarioId])
         const todosOsDados = pegarDados.rows
-
-
-        const id = todosOsDados[0].id
-        const shortUrl = todosOsDados[0].urlEncurtada
-        const url = todosOsDados[0].urlOriginal
-       
-
-        const totalLinks = pegarDados.rowCount
-
-        const allDados = nomeUsuario.rows.map(all => {
-            const id = confirmToken.rows.find(i => i.idUsuario)
-            const name = nomeUsuario.rows.find(n => n.name)
-
-            return {
-                id: id.idUsuario,
-                name: name.name,
-                visitCount: 0,
-                shortenedUrls: todosOsDados
-            }
-        })
-        
-
-        let somaVisitas = 0;
-        allDados[0].shortenedUrls.forEach(url => {
-        somaVisitas += url.contagemVisitas;
-        allDados[0].visitCount = somaVisitas
-    })
 
     const objeto = {
         id: confirmToken.rows[0].idUsuario, 
